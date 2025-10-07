9e00d4d13d1a4cb7ee4fa9cd0fe682df1e4116dc
Hilmy Haidar <84982183+fm146@users.noreply.github.com>
2025-10-07 23:39:06 +0700

    Delete Downloads/Interactive Footprint Chart directory

--- Downloads/Interactive Footprint Chart/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { FootprintChart } from './components/FootprintChart';
-
-export default function App() {
-  return (
-    <div className="w-full h-screen">
-      <FootprintChart />
-    </div>
-  );
-}
