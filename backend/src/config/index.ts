8a2307da1600b7783dc44d805e5d044fb8593dbb
Ubuntu <bandarmolony@bandarmolony.4spnfdijyexepfdxp2vw5vlvyb.bx.internal.cloudapp.net>
2025-11-07 09:20:00 +0000

    fix: ci/cd flow

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -7,7 +7,6 @@ export const config = {
   
   // Database
   SUPABASE_URL: process.env['SUPABASE_URL'] || '',
-  SUPABASE_ANON_KEY: process.env['SUPABASE_ANON_KEY'] || '',
   SUPABASE_SERVICE_ROLE_KEY: process.env['SUPABASE_SERVICE_ROLE_KEY'] || '',
   
   // Midtrans
